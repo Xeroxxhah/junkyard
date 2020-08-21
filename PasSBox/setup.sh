ba1b8b3fa018d247933570098642b20ecdcbf99f
Xeroxxhah <43526417+Xeroxxhah@users.noreply.github.com>
2020-08-21 22:13:59 +0500

    Update setup.sh

--- PasSBox/setup.sh
+++ PasSBox/setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 echo 'Setting up PasSBox'
-
+if [ $(id -u) != '0'  ]
+then
+  echo 'This Script must be run as root'
+exit
+else
   mkdir .PasSBox
   touch .PasSBox/users.txt
   clear
   echo 'Setup Sucessfull'
   echo 'Syntax: python3 PasSBox.py'
   echo 'Bug Report at xeroxxhah@pm.me'
+  echo '*Run Program as root*'
+fi
